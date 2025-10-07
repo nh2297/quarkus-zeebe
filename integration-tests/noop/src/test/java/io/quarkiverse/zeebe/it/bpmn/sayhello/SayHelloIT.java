68cf28e6300f38b5fd14a1e59577b827d6a8ea51
Nico Hinrichs <n_hinrichs@taures.de>
2025-10-07 23:10:40 +0200

    remove test profile

--- integration-tests/noop/src/test/java/io/quarkiverse/zeebe/it/bpmn/sayhello/SayHelloIT.java
+++ integration-tests/noop/src/test/java/io/quarkiverse/zeebe/it/bpmn/sayhello/SayHelloIT.java
@@ -4,6 +4,5 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.quarkus.test.junit.TestProfile;
 
 @QuarkusIntegrationTest
-@TestProfile(value = io.quarkiverse.zeebe.it.bpmn.sayhello.TestProfile.class)
 public class SayHelloIT extends SayHelloTest {
 }
